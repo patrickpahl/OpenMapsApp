f19fe677c33c41de6b8e7d3729fdd6a003eca99b
Patrick <patrick.pahl@loves.com>
2018-10-01 17:00:08 -0500

    Initial Commit

---
+++ OpenMapsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenMapsApp
+//
+//  Created by Patrick Pahl on 10/1/18.
+//  Copyright © 2018 Patrick Pahl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
